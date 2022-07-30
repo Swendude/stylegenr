fcfe2dc3f889955e8d1f7afe046d63427b9d6d6d
Swen Mulderij <swenmulderij@gmail.com>
2022-07-30 21:44:23 +0200

    hspec dependencies seem to work

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,11 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+splitWords :: String -> [String]
+splitWords s = splitWords' s ""
+
+splitWords' :: String -> String -> [String]
+splitWords' (h : inp_t) stack
+  | h == ' ' && stack /= "" = stack : splitWords' inp_t ""
+  | h == ' ' && stack == "" = splitWords' inp_t ""
+  | otherwise = splitWords' inp_t (stack ++ [h])
+splitWords' [] stack = [stack]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+module Main where
+
+import Lib (splitWords)
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Test if splitWords works" $ do
+    it "splitWords should split a string into words" $ do
+      splitWords "thanks for all the fish" `shouldBe` ["thanks", "for", "all", "the", "fish"]
+    it "splitWords should split a string into words ignoring multiple spaces" $ do
+      splitWords " thanks  for   all    the fish" `shouldBe` ["thanks", "for", "all", "the", "fish"]
+  -- describe "Test if replaceWhitespace works" $ do
+  --   it "replaceWhitespace replaces all whitespace" $ do
+  --     replaceWhitespace "/nthanks/t /tfor /n all/n the fish" `shouldBe` "thanks for all the fish"
