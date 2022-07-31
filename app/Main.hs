08cec3d7f5dd2c587da9e001397a7bf4e0947b1d
Swen Mulderij <swenmulderij@gmail.com>
2022-07-31 21:45:51 +0200

    Textgen works

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,14 @@ module Main where
 
 import GHC.IO.FD (openFile)
 import GHC.IO.IOMode (IOMode (ReadMode))
+import Stylegen (styleGen)
 import System.Environment (getArgs)
+import System.Random
 import TextCleaning (cleanText, splitWords)
 
 main :: IO ()
 main = do
   fn <- getArgs
   fc <- readFile (head fn)
-  print $ splitWords $ cleanText fc
+  gen <- getStdGen
+  print (styleGen gen (splitWords $ cleanText fc) 400)
--- src/Stylegen.hs
+++ src/Stylegen.hs
@@ -1,11 +1,29 @@
 module Stylegen where
 
-import Data.Map (Map, empty, insertWith)
+import Data.List (intersperse)
+import Data.Map (Map, empty, insertWith, keys, lookup)
+import Data.Maybe (fromJust, isJust, isNothing)
+import System.Random
 
 type Gendict = Map String [String]
 
-styleGen :: [String] -> Int -> String
-styleGen words count = concat $ take count $ repeat $ head words
+styleGen :: StdGen -> [String] -> Int -> String
+styleGen gen words count =
+  let dict = buildDict words
+      seed = random gen :: (Int, StdGen)
+      start = fst $ (random (mkStdGen (fst seed)) :: (Int, StdGen))
+      wordgen = snd seed
+      first = keys dict !! (start `mod` length dict)
+   in concat $ intersperse " " $ take count $ selectWord wordgen first dict
+
+selectWord :: StdGen -> String -> Gendict -> [String]
+selectWord gen choice dict
+  | isJust (Data.Map.lookup choice dict) =
+      let options = fromJust $ Data.Map.lookup choice dict
+          rd = random gen
+          newWord = options !! ((fst rd) `mod` length options)
+       in newWord : selectWord (snd rd) newWord dict
+  | isNothing (Data.Map.lookup choice dict) = []
 
 buildDict :: [String] -> Gendict
 buildDict (h : t) = foldr addToDict empty $ zip (h : t) t
